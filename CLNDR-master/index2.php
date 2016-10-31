434ecb1135cbda1c87b44c2ca2f893882e17f670
Jessica <jessicafndez@gmail.com>
2016-10-31 17:58:31 +0100

    Update

---
+++ CLNDR-master/index2.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>CLNDR Demo</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/clndr.css">
+</head>
+<body>
+    <div class="">
+        <div class="cal1"></div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+    <script src="../../calendar/moment.js"></script>
+    <script src="../../calendar/moment-with-locales.js"></script>
+    <script src="events-list.js"></script>
+    <script src="../src/clndr.js"></script>
+    <script src="demo2.js"></script>
+</body>
+</html>
